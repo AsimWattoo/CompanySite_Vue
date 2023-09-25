5a39709d72b008a0b7312cb1051d36312a0cec30
AsimWattoo <asimwattoo6@gmail.com>
2023-09-25 18:09:10 +0500

    Made the Menu bar responsive by adding a button when screen size decreases from 960 and hiding the menu bar

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -3,24 +3,32 @@
         <div class="logo">
             <img src="../assets/images/logo.png" alt="logo"/>
         </div>
-        <div class="menu-items">
-            <div class="menu-item active">
-                Home
+        <div class="menu-items-container">
+            <div class="menu-button">
+                <font-awesome-icon icon="bars" />
             </div>
-            <div class="menu-item">
-                Services
-            </div>
-            <div class="menu-item">
-                Pricing
-            </div>
-            <div class="menu-item">
-                Products
-            </div>
-            <div class="menu-item">
-                About Us
-            </div>
-            <div class="menu-item">
-                Contact Us
+            <div class="menu-items">
+                <div class="menu-item active">
+                    Home
+                </div>
+                <div class="menu-item">
+                    Services
+                </div>
+                <div class="menu-item">
+                    Pricing
+                </div>
+                <div class="menu-item">
+                    Products
+                </div>
+                <div class="menu-item">
+                    About Us
+                </div>
+                <div class="menu-item outline">
+                    <div class="outline-bg"></div>
+                    <div class="text">
+                        Contact Us
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -37,11 +45,13 @@ export default {
 .menu-container {
     padding: 1em 2em;
     box-shadow: rgba(0, 0, 0, 0.05) 0px 1px 2px 0px;
+    background-color: black;
     border-radius: 0 0 10px 10px;
     display: flex;
     flex-direction: row;
     align-items: center;
     justify-content: space-between;
+    overflow: hidden;
 }
 
 .logo {
@@ -51,17 +61,34 @@ export default {
     }
 }
 
+.menu-items-container {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    justify-content: end;
+}
+
+.menu-button {
+    color: white;
+    font-size: 25px;
+    visibility: collapse;
+    height: 0;
+    margin-right: 0.5em;
+}
+
 .menu-items {
     display: flex;
+    width: 100%;
     align-items: center;
     justify-content: end;
 
     .menu-item {
         font-weight: 300;
         font-size: 25px;
-        padding: 10px 15px;
+        padding: 8px 15px;
         border-radius: 5px;
         cursor: pointer;
+        color: white;
         background-color: transparent;
         transition: all 0.3s cubic-bezier(0.075, 0.82, 0.165, 1);
         position: relative;
@@ -70,10 +97,32 @@ export default {
             color: #40c6b1;
         }
 
-        &::before {
+        &.outline {
+            border: 2px solid #51fadf;
+            border-radius: 10px;
+            overflow: hidden;
+
+            .outline-bg {
+                position: absolute;
+                top: 100%;
+                left: 0%;
+                background-color: #51fadf;
+                width: 100%;
+                height: 100%;
+                z-index: 1;
+                transition: all 0.3s cubic-bezier(0.075, 0.82, 0.165, 1);
+            }
+
+            .text {
+                position: relative;
+                z-index: 2;
+            }
+        }
+
+        &:not(.outline)::before {
             content: '';
             position: absolute;
-            top: 100%;
+            top: 98%;
             left: 50%;
             width: 0;
             background-color: #51fadf;
@@ -83,12 +132,44 @@ export default {
         }
 
         &:hover {
-            &::before {
+            &:not(.outline)::before {
                 transform: translateX(-50%);
-                width: 80%;
+                width: 65%;
+            }
+
+            &.outline {
+                color: black;
+                .outline-bg{
+                    top: 0%;
+                }
             }
         }
+    }
+}
 
+@media only screen and (max-width: 960px) {
+
+    .menu-container {
+        padding: 0.3em 0.5em;
+
+        img {
+            height: 25px;
+            width: 25px;
+        }
+    }
+
+    .menu-items-container {
+        width: 75%;
+    }
+
+    .menu-items {
+        visibility: hidden;
+        width: 0%;
+    }
+
+    .menu-button {
+        visibility: visible;
+        height: auto;
     }
 }
 
--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 let app = createApp(App)
 
+library.add(faBars)
 // Adding use statement
 app.use(router)
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 // Building the app
 app.mount('#app')
